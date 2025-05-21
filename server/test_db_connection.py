cfbbba75b520667a1da783c8c4ca931e94d1655b
LilyBurton <lily.burton1992@gmail.com>
2025-05-21 19:01:53 +0100

    tidying up

--- server/test_db_connection.py
+++ server/test_db_connection.py
@@ -2,8 +2,6 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import OperationalError
 
 # Replace this with your actual URI from Render
-# DATABASE_URL = "postgresql+psycopg2://lil:secretpassword@fanfiction-db-vq9j.onrender.com:5432/fanfiction?sslmode=require"
-DATABASE_URL="postgresql+psycopg2://fanfiction_db_user:srVlUQfXk2uKLBoDnBMWMnrxSJKT9lUJ@dpg-d0m9jlidbo4c73cl1tgg-a.frankfurt-postgres.render.com:5432/fanfiction_db?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 
